874c862f85e0323e426f59b1a58b5d2be4e613d3
nisargshah1110 <nisarg633@gmail.com>
2026-03-29 15:08:06 +0530

    error chck

--- valuemodel-marketplace/next.config.ts
+++ valuemodel-marketplace/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  onDemandEntries: {
+    maxInactiveAge: 60 * 1000,
+    pagesBufferLength: 5,
+  },
 };
 
 export default nextConfig;
