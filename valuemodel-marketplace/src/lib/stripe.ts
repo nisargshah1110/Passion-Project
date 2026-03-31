efa1b3eac42a74ca77099198bacaeb9864beac1a
nisargshah1110 <nisarg633@gmail.com>
2026-03-31 15:41:33 +0530

    Update stripe.ts

--- valuemodel-marketplace/src/lib/stripe.ts
+++ valuemodel-marketplace/src/lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from "stripe";
 // Temporarily disabled for testing - will be enabled when env variables are set
 export const stripe = process.env.STRIPE_SECRET_KEY 
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2024-11-08",
+      apiVersion: "2023-10-16",
     })
   : null;
